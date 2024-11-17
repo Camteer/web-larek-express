6c0ecdfa48ecc5e25319219f23175198c5d16230
Pavlov Evgeniy <pavlov2001@bk.ru>
2024-11-17 18:02:40 +0300

    fix: middlewares

--- backend/src/middlewares/error-handler.ts
+++ backend/src/middlewares/error-handler.ts
@@ -1,4 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
-export default (err: any, _req: Request, res: Response, _next: NextFunction) =>
+export default function (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
   res.status(err.statusCode).send({ message: err.message });
+}
